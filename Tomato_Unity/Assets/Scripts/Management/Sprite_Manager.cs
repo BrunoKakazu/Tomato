e4a47e820e968b2762a056cb7f4e52a8ed55e849
Bruno_Kakazu <brunokakazu98@gmail.com>
2025-02-04 19:29:05 -0300

    Criação de Telas
    Criando as telas para depois fazer os scripts

---
+++ Tomato_Unity/Assets/Scripts/Management/Sprite_Manager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Sprite_Manager : MonoBehaviour
+{
+    [SerializeField] private Scriptable_Object _scriptableObject;
+
+    
+
+}
---
+++ Tomato_Unity/Assets/Scripts/UI/Scriptable_Object.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Scriptable Object", menuName = "Scriptable Object")]
+public class Scriptable_Object : ScriptableObject
+{
+    public List<Sprite> spriteList;
+}
